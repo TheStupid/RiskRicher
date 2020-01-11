46cc1c57f0e1913528d1759a71ece2609a4ba4ac
TheStupid <tobehappy0@126.com>
2020-01-11 16:02:58 +0800

    这一步好像也没问题

---
+++ src/config/BaseConfig.ts
@@ -0,0 +1,16 @@
+import CommonRes from "./CommonRes";
+
+export default class BaseConfig {
+    private static readonly GAME_CH_NAME:string = "魔物讨伐者";
+    public static readonly MIN_LEVEL:number = 1;
+    public static readonly MAX_LEVEL:number = 999;
+    public static readonly MIN_DAMAGE:number = 0;
+
+    private static readonly NECESSARY_FILES:Array<string> = [
+        CommonRes.URL_COMMON,
+        CommonRes.URL_IMG_BLACK
+    ];
+    public static getNecessaryFiles():Array<string>{
+        return BaseConfig.NECESSARY_FILES;
+    }
+}
